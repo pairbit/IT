bfdb29c58fc09a6356d621f032b5698dda57c889
Ivan Tikhonov <itikhonov@mail.com>
2022-04-11 11:41:13 +0300

    add _String.Contains

--- src/IT.Bcl/_String.cs
+++ src/IT.Bcl/_String.cs
@@ -7,11 +7,24 @@ public static class _String
     public static String[] Split(this String value, String separator, StringSplitOptions options = StringSplitOptions.None)
         => value.Split(new[] { separator }, options);
 
-    public static Boolean StartsWith(this String value, Char ch) => value.StartsWith(ch.ToString());
+    public static Boolean StartsWith(this String value, Char ch) => value.Length != 0 && value[0] == ch;
 
-    public static Boolean EndsWith(this String value, Char ch) => value.EndsWith(ch.ToString());
+    public static Boolean EndsWith(this String value, Char ch)
+    {
+        var lastPos = value.Length - 1;
+        return lastPos >= 0 && value[lastPos] == ch;
+    }
 
-    public static Boolean Contains(this String value, Char ch) => value.Contains(ch.ToString());
+    public static Boolean Contains(this String value, String str, StringComparison comparisonType)
+        => value.IndexOf(str, comparisonType) >= 0;
+
+    public static Boolean Contains(this String value, Char ch) => value.IndexOf(ch) >= 0;
+
+    public static Boolean Contains(this String value, Char ch, StringComparison comparisonType)
+        => value.IndexOf(ch.ToString(), comparisonType) >= 0;
+
+    public static Int32 IndexOf(this String value, Char ch, StringComparison comparisonType)
+        => value.IndexOf(ch.ToString(), comparisonType);
 }
 
 #endif
--- tests/IT.Tests/UnitTest1.cs
+++ tests/IT.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace IT.Tests
 {
@@ -14,8 +15,30 @@ namespace IT.Tests
         public void Test1()
         {
             //Assert.Pass();
-            //Assert.AreEqual(Class1.Test, "Test");
+
+            var str = "abcdez";
+
+            Assert.AreEqual(str.StartsWith('a'), _StartsWith(str, 'a'));
+
+            Assert.AreEqual(str.EndsWith('z'), _EndsWith(str, 'z'));
+
+            str = "";
+
+            Assert.AreEqual(str.EndsWith('z'), _EndsWith(str, 'z'));
+
+            str = "z";
+
+            Assert.AreEqual(str.EndsWith('z'), _EndsWith(str, 'z'));
+
             //Assert.AreEqual(Class1.Test2, 2345234);
+        }
+
+        public static Boolean _StartsWith(String value, Char ch) => value.Length != 0 && value[0] == ch;
+
+        public static Boolean _EndsWith(String value, Char ch)
+        {
+            var lastPos = value.Length - 1;
+            return lastPos >= 0 && value[lastPos] == ch;
         }
     }
 }
