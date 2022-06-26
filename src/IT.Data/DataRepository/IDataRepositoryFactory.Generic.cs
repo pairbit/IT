b9191639bda91a2392eb62cc5de11b8d5761188c
Ivan Tikhonov <itikhonov@mail.com>
2022-06-26 16:36:22 +0300

    add data redis

--- src/IT.Data/DataRepository/IDataRepositoryFactory.Generic.cs
+++ src/IT.Data/DataRepository/IDataRepositoryFactory.Generic.cs
@@ -1,13 +1,20 @@
-﻿using IT.Serialization;
-using System;
+﻿using System;
 
 namespace IT.Data;
 
 public interface IDataRepositoryFactory<TId, TValue>
 {
-    IDataRepository<TId, TValue>? GetRepository(String name, String? connectionString = null,
-        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+    IDataRepository<TId, TValue>? Get(String name,
+        Func<TId>? newId = null,
+        Func<TId, Byte[]>? idSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TId?>? idDeserialize = null,
+        Func<TValue, Byte[]>? valueSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TValue?>? valueDeserialize = null);
 
-    IAsyncDataRepository<TId, TValue>? GetAsyncRepository(String name, String? connectionString = null,
-        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+    IAsyncDataRepository<TId, TValue>? GetAsync(String name,
+        Func<TId>? newId = null,
+        Func<TId, Byte[]>? idSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TId?>? idDeserialize = null,
+        Func<TValue, Byte[]>? valueSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TValue?>? valueDeserialize = null);
 }
--- src/IT.Data/DataRepository/IDataRepositoryFactory.cs
+++ src/IT.Data/DataRepository/IDataRepositoryFactory.cs
@@ -1,13 +1,20 @@
-﻿using IT.Serialization;
-using System;
+﻿using System;
 
 namespace IT.Data;
 
 public interface IDataRepositoryFactory
 {
-    IDataRepository<TId, TValue>? GetRepository<TId, TValue>(String name, String? connectionString = null,
-        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+    IDataRepository<TId, TValue>? Get<TId, TValue>(String name,
+        Func<TId>? newId = null,
+        Func<TId, Byte[]>? idSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TId?>? idDeserialize = null,
+        Func<TValue, Byte[]>? valueSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TValue?>? valueDeserialize = null);
 
-    IAsyncDataRepository<TId, TValue>? GetAsyncRepository<TId, TValue>(String name, String? connectionString = null,
-        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+    IAsyncDataRepository<TId, TValue>? GetAsync<TId, TValue>(String name,
+        Func<TId>? newId = null,
+        Func<TId, Byte[]>? idSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TId?>? idDeserialize = null,
+        Func<TValue, Byte[]>? valueSerialize = null,
+        Func<ReadOnlyMemory<Byte>, TValue?>? valueDeserialize = null);
 }
