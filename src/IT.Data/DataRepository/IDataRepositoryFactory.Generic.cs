fdc17528858f320dcffade7f06fa51615e3114a3
Ivan Tikhonov <itikhonov@mail.com>
2022-06-26 12:52:33 +0300

    add Data

---
+++ src/IT.Data/DataRepository/IDataRepositoryFactory.Generic.cs
@@ -0,0 +1,13 @@
+﻿using IT.Serialization;
+using System;
+
+namespace IT.Data;
+
+public interface IDataRepositoryFactory<TId, TValue>
+{
+    IDataRepository<TId, TValue>? GetRepository(String name, String? connectionString = null,
+        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+
+    IAsyncDataRepository<TId, TValue>? GetAsyncRepository(String name, String? connectionString = null,
+        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+}
---
+++ src/IT.Data/DataRepository/IDataRepositoryFactory.cs
@@ -0,0 +1,13 @@
+﻿using IT.Serialization;
+using System;
+
+namespace IT.Data;
+
+public interface IDataRepositoryFactory
+{
+    IDataRepository<TId, TValue>? GetRepository<TId, TValue>(String name, String? connectionString = null,
+        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+
+    IAsyncDataRepository<TId, TValue>? GetAsyncRepository<TId, TValue>(String name, String? connectionString = null,
+        Func<TId>? newId = null, ISerializer<TId>? idSerializer = null, ISerializer<TValue>? valueSerializer = null);
+}
