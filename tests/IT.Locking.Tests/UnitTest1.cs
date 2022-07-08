8a7d1030b7d8c75a65b0fc0a75691efcece8c560
Ivan Tikhonov <itikhonov@mail.com>
2022-07-08 12:18:38 +0300

    add tests and benchmarks

---
+++ tests/IT.Locking.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IT.Locking.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
