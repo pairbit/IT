a00f387ee0e84f1be4318bed9ab7447ce16fbd50
Ivan Tikhonov <itikhonov@mail.com>
2022-07-08 11:31:40 +0300

    add seri benchmarks

--- benchmarks/IT.Serialization.Benchmarks/SerializeBenchmark.cs
+++ benchmarks/IT.Serialization.Benchmarks/SerializeBenchmark.cs
@@ -15,16 +15,20 @@ public class SerializeBenchmark
     protected static readonly Object _personObject = _generator.Generate(typeof(Person));
 
     private readonly ISerializer _jsonSerializer;
-    private readonly byte[] _personJsonSerialize;
+    private readonly byte[] _jsonSerializerBytes;
     private readonly ISerializer _messagePackSerializer;
-    private readonly byte[] _personMessagePackSerialize;
+    private readonly byte[] _messagePackSerializerBytes;
+    private readonly ISerializer _utf8jsonSerializer;
+    private readonly byte[] _utf8jsonSerializerBytes;
 
     public SerializeBenchmark()
     {
         _jsonSerializer = new Json.TextSerializer();
-        _personJsonSerialize = Json_Serialize();
+        _jsonSerializerBytes = Json_Serialize();
         _messagePackSerializer = new MessagePack.Serializer();
-        _personMessagePackSerialize = MessagePack_Serialize();
+        _messagePackSerializerBytes = MessagePack_Serialize();
+        _utf8jsonSerializer = new Utf8Json.TextSerializer();
+        _utf8jsonSerializerBytes = Utf8Json_Serialize();
     }
 
     [Benchmark]
@@ -34,24 +38,31 @@ public class SerializeBenchmark
     public byte[] MessagePack_Serialize() => _messagePackSerializer.Serialize(_person);
 
     [Benchmark]
-    public Person? Json_Deserialize() => _jsonSerializer.Deserialize<Person>(_personJsonSerialize);
+    public byte[] Utf8Json_Serialize() => _utf8jsonSerializer.Serialize(_person);
 
     [Benchmark]
-    public Person? MessagePack_Deserialize() => _messagePackSerializer.Deserialize<Person>(_personMessagePackSerialize);
+    public Person? Json_Deserialize() => _jsonSerializer.Deserialize<Person>(_jsonSerializerBytes);
 
     [Benchmark]
-    public void Json()
+    public Person? MessagePack_Deserialize() => _messagePackSerializer.Deserialize<Person>(_messagePackSerializerBytes);
+
+    [Benchmark]
+    public Person? Utf8Json_Deserialize() => _utf8jsonSerializer.Deserialize<Person>(_utf8jsonSerializerBytes);
+
+    [Benchmark]
+    public Person Json() => SerializeDeserialize(_jsonSerializer);
+
+    [Benchmark]
+    public Person MessagePack() => SerializeDeserialize(_messagePackSerializer);
+
+    [Benchmark]
+    public Person Utf8Json() => SerializeDeserialize(_utf8jsonSerializer);
+
+    private Person SerializeDeserialize(ISerializer serializer)
     {
-        var bytes = _jsonSerializer.Serialize(_person);
-        var person = _jsonSerializer.Deserialize<Person>(bytes);
-        if (!_person.Equals(person)) throw new InvalidOperationException();
-    }
-
-    [Benchmark]
-    public void MessagePack()
-    {
-        var bytes = _messagePackSerializer.Serialize(_person);
-        var person = _messagePackSerializer.Deserialize<Person>(bytes);
-        if (!_person.Equals(person)) throw new InvalidOperationException();
+        var bytes = serializer.Serialize(_person);
+        var person = serializer.Deserialize<Person>(bytes);
+        //if (!_person.Equals(person)) throw new InvalidOperationException();
+        return person!;
     }
 }
