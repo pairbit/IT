c8f16c6ca08fa4abbbcdf68d45114ef5d0214e91
Ivan Tikhonov <itikhonov@mail.com>
2022-07-09 18:49:02 +0300

    change test lock

---
+++ tests/IT.Locking.Tests/LockRedisTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Locking.Tests;
+
+internal class LockRedisTest
+{
+}
--- tests/IT.Locking.Tests/LockTest.cs
+++ tests/IT.Locking.Tests/LockTest.cs
@@ -1,18 +1,15 @@
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 
 namespace IT.Locking.Tests;
 
-public class NoLockTest
+public class LockTest
 {
     private readonly Random _random = new();
 
     [Test]
-    public Task NoLock() => Parallel(InsertData);
+    public Task Test() => Parallel(InsertData);
 
-    [Test]
-    public Task NoLockWithCheck() => Parallel(InsertDataWithCheck);
-
-    private async Task Parallel(Action<IDictionary<Guid, Byte>> action)
+    protected async Task Parallel(Action<IDictionary<Guid, Byte>> action)
     {
         var data = new ConcurrentDictionary<Guid, Byte>();
         var tasks = new Task[10];
@@ -38,20 +35,11 @@ public class NoLockTest
         }
     }
 
-    private void InsertData(IDictionary<Guid, Byte> data)
+    private void InsertData(IDictionary<Guid, Byte> data) 
+        => InsertData(data, (byte)_random.Next(0, 256));
+
+    protected virtual void InsertData(IDictionary<Guid, Byte> data, Byte value)
     {
-        var value = (Byte)_random.Next(0, 256);
         data.Add(Guid.NewGuid(), value);
-    }
-
-    private void InsertDataWithCheck(IDictionary<Guid, Byte> data)
-    {
-        var value = (Byte)_random.Next(0, 256);
-
-        if (!data.Values.Contains(value))
-        {
-            Task.Delay(10).Wait();
-            data.Add(Guid.NewGuid(), value);
-        }
     }
 }
