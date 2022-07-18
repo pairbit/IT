2bfe8e0cc6e212610cc8f1f5755eeb8705942d13
Ivan Tikhonov <itikhonov@mail.com>
2022-07-18 12:52:07 +0300

    add interfaces

--- src/IT.Encoding/IEncoder.cs
+++ src/IT.Encoding/IEncoder.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Buffers;
 
 namespace IT.Encoding;
 
 public interface IEncoder
 {
+    Int32 MaxEncodedLength { get; }
 
+    Int32 GetMaxEncodedLength(Int32 dataLength);
+
+    Int32 GetMaxDecodedLength(Int32 encodedLength);
+
+    Int32 GetEncodedLength(ReadOnlySpan<Byte> data);
+
+    Int32 GetDecodedLength(ReadOnlySpan<Byte> encoded);
+
+    OperationStatus Encode(ReadOnlySpan<Byte> data, Span<Byte> encoded, out Int32 consumed, out Int32 written, Boolean isFinal = true);
+
+    OperationStatus Decode(ReadOnlySpan<Byte> encoded, Span<Byte> data, out Int32 consumed, out Int32 written, Boolean isFinal = true);
+
+    OperationStatus Encode(Span<Byte> buffer, Int32 length, out Int32 written);
+
+    OperationStatus Decode(Span<Byte> buffer, out Int32 written);
 }
--- src/IT.Encoding/ITextEncoder.cs
+++ src/IT.Encoding/ITextEncoder.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Buffers;
 
 namespace IT.Encoding;
 
 public interface ITextEncoder : IEncoder
 {
+    Int32 GetDecodedLength(ReadOnlySpan<Char> encoded);
+
+    OperationStatus Encode(ReadOnlySpan<Byte> data, Span<Char> encoded, out Int32 consumed, out Int32 written, Boolean isFinal = true);
+
+    OperationStatus Decode(ReadOnlySpan<Char> encoded, Span<Byte> data, out Int32 consumed, out Int32 written, Boolean isFinal = true);
+
     String Encode(ReadOnlySpan<Byte> data);
 
     Byte[] Decode(ReadOnlySpan<Char> encoded);
