533d9baf432b0f4467507b4d2e77efea94445962
Ivan Tikhonov <itikhonov@mail.com>
2022-07-18 09:53:33 +0300

    add Encoding and Compression

---
+++ src/IT.Encoding/ITextEncoder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IT.Encoding;
+
+public interface ITextEncoder : IEncoder
+{
+    String Encode(ReadOnlySpan<Byte> data);
+
+    Byte[] Decode(ReadOnlySpan<Char> encoded);
+}
