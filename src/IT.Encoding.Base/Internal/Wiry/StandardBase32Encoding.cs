75bb5d9e88d6ab759f67ed3903a644d024f7891e
Ivan Tikhonov <itikhonov@mail.com>
2022-07-22 16:54:43 +0300

    add Wiry

---
+++ src/IT.Encoding.Base/Internal/Wiry/StandardBase32Encoding.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dmitry Razumikhin, 2016-2019.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Wiry.Base32
+{
+    internal sealed class StandardBase32Encoding : Base32Encoding
+    {
+        protected override string Alphabet => "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+        protected override char? PadSymbol => '=';
+    }
+}
---
+++ src/IT.Encoding.Base/Internal/Wiry/ZBase32Encoding.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Dmitry Razumikhin, 2016-2019.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Wiry.Base32
+{
+    internal sealed class ZBase32Encoding : Base32Encoding
+    {
+        protected override string Alphabet => "ybndrfg8ejkmcpqxot1uwisza345h769";
+        protected override char? PadSymbol => null;
+    }
+}
