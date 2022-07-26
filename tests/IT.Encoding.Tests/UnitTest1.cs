a0a9690fa7b2d8ca21364b44d299703fece11bda
Ivan Tikhonov <itikhonov@mail.com>
2022-07-26 11:15:58 +0300

    add IT.Encoding.Web

--- tests/IT.Encoding.Tests/UnitTest1.cs
+++ tests/IT.Encoding.Tests/UnitTest1.cs
@@ -1,16 +1,19 @@
-namespace IT.Encoding.Tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace IT.Encoding.Tests;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var htmlEncoder = System.Text.Encodings.Web.HtmlEncoder.Default;
+
+        Assert.That(htmlEncoder.Encode("<p>My text</p>"), Is.EqualTo("&lt;p&gt;My text&lt;/p&gt;"));
+
+
     }
 }
