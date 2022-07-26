2a4e51ea0068713d3934ef9db5f4c838cd19c5e4
Ivan Tikhonov <itikhonov@mail.com>
2022-07-26 12:21:13 +0300

    add

---
+++ src/IT.Encoding.Web/WebEncoder.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace IT.Encoding.Web;
+
+//public class WebEncoder : TextEncoder
+//{
+//    private readonly System.Text.Encodings.Web.TextEncoder _textEncoder;
+
+//    public WebEncoder(System.Text.Encodings.Web.TextEncoder textEncoder)
+//    {
+//        _textEncoder = textEncoder;
+//    }
+
+
+//}
--- tests/IT.Encoding.Tests/UnitTest1.cs
+++ tests/IT.Encoding.Tests/UnitTest1.cs
@@ -8,9 +8,16 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void HtmlTest()
     {
         var htmlEncoder = System.Text.Encodings.Web.HtmlEncoder.Default;
+
+        // "&#xFFFF;" is worst case for single char ("&#x10FFFF;" [10 chars] worst case for arbitrary scalar value)
+        var maxEn = htmlEncoder.MaxOutputCharactersPerInputCharacter;
+
+        var encoded = htmlEncoder.Encode("ÿ");
+
+        Assert.That(htmlEncoder.Encode("ÿ"), Is.EqualTo("&#x44F;"));
 
         Assert.That(htmlEncoder.Encode("<p>My text</p>"), Is.EqualTo("&lt;p&gt;My text&lt;/p&gt;"));
 
