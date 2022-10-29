6b73f63ee98da12922c4fc74fc3bb8b9e7e1d689
Ivan Tikhonov <itikhonov@mail.com>
2022-10-29 21:25:35 +0300

    add Id MinValue and MaxValue

--- src/IT.Id/Id.cs
+++ src/IT.Id/Id.cs
@@ -19,6 +19,8 @@ public readonly struct Id : IComparable<Id>, IEquatable<Id>, IFormattable
     private static readonly Int64 _random = CalculateRandomValue();
     private static Int32 _staticIncrement = new Random().Next();
     public static readonly Id Empty = default;
+    public static readonly Id MinValue = new(0, 0, 0);
+    public static readonly Id MaxValue = new(-1, -1, -1);
 
     internal readonly Int32 _timestamp;
     internal readonly Int32 _b;
