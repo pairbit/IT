f5f109db8c8590ceb8d855d08dbefc2a9bc01215
Ivan Tikhonov <itikhonov@mail.com>
2022-12-09 11:03:30 +0300

    move IT.Locking to https://github.com/pairbit/IT.Locking

--- src/IT.Locking.Redis/StringDeleteIfEqual.lua
+++
@@ -1,5 +0,0 @@
-﻿if redis.call('get', KEYS[1]) == ARGV[1] then
-	return redis.call('del', KEYS[1])
-else
-	return 0
-end
--- src/IT.Locking/IAsyncLocked.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IT.Locking;
-
-public interface IAsyncLocked : IAsyncDisposable
-{
-
-}
